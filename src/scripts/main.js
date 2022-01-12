df5c57f97e6a87b02daae673306bcf999d460182
Valeria Valuyskaya <valeriavaluyskaya@gmail.com>
2022-01-12 13:36:08 +0200

    miami

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const form = document.querySelector('.follow__form');
+const inputs = document.querySelectorAll('.contact-us__field');
+const textarea = document.querySelector('.contact-us__form-text');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+
+  textarea.value = '';
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
